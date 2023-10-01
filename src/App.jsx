5365e9ff493e76a10a91e34e6a697cafd1a5ba41
ishneeeeeet <ishneet100@gmail.com>
2023-10-01 01:46:01 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react";
+import "./App.css";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import { Outlet } from "react-router-dom";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <>
+      <h1 className="">
+        <Header/>
+        <main>
+          {/* <Outlet/> */}
+        </main>
+        
+        <Footer/>
+      </h1>
+    </>
+  ) : null;
+}
+
+export default App;
