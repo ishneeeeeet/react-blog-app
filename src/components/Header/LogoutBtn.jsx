0568e6f4e6f2dc3794614ee4c6e92fb06b5f4813
ishneeeeeet <ishneet100@gmail.com>
2023-10-01 18:47:44 -0600

    second commit

---
+++ src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import authService from "../../appwrite/config";
+import { logout } from "../../store/authSlice";
+
+const LogoutBtn = () => {
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {
+    authService.logout().then(() => {
+        dispatch(logout())
+    })
+  };
+  return <button onClick={logoutHandler}>Logout</button>
+};
+
+export default LogoutBtn;
